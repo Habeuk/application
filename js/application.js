14db5ab4c0189fa6cdcad65025d47093900ef7b5
kouwa stephane <kksasteph888@gmail.com>
2024-01-02 12:50:14 +0100

    Maj

--- js/application.js
+++ js/application.js
@@ -1,6 +1,5 @@
 $(function() {
     $('li.dropdown').each(function( index, element ) {
-        console.log(element);
         $(element).on("mouseenter", function() {
             $(element).find('.sub-menu').removeClass('hide');
         }).on("mouseleave", function() {
